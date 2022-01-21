b2d3b19ba3588dd5e2f5044d772ec8565f5ddd40
mohsen <m.hariri@gmail.com>
2022-01-20 23:25:03 -0800

    Add description for Spotify

--- print.js
+++ print.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async() => {
     const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
-    await page.goto('http://localhost:8000/index.html', { waitUntil: 'networkidle2' });
+    await page.goto('http://host.docker.internal:8000/index.html', { waitUntil: 'networkidle2' });
     await page.pdf({
         path: '/rr/resume.pdf',
         format: 'A4',
